aec4bfe061f7e540ea12903a32b913b41ab727cb
Joel Nicolas Luna <nnicolasluna@gmail.com>
2025-06-12 15:46:37 -0400

    implementacion de controls en el formulario

--- src/app/modules/meets/pages/agenda/components/form-actividades/form-actividades.component.ts
+++ src/app/modules/meets/pages/agenda/components/form-actividades/form-actividades.component.ts
@@ -1,27 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, QueryList, ViewChildren } from '@angular/core';
 import { FormTareasComponent } from '../form-tareas/form-tareas.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
-import { FormGroup } from '@angular/forms';
+import { ChangeDetectorRef } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { SelectModule } from 'primeng/select';
+import { BaseFormComponent } from '@shared/components/abstracts/base-form.component';
+import { UserModel } from '@modules/users/interfaces';
+import { UserService } from '@modules/users/services/user.service';
+import { InputErrorComponent, InputTextComponent } from '@shared/components';
 
 @Component({
 	selector: 'app-form-actividades',
-	imports: [FormsModule, CommonModule, ButtonModule, FormTareasComponent, InputTextModule, SelectModule],
+	imports: [InputErrorComponent, ReactiveFormsModule, InputTextComponent, FormsModule, CommonModule, ButtonModule, FormTareasComponent, InputTextModule, SelectModule],
 	templateUrl: './form-actividades.component.html',
 	styleUrl: './form-actividades.component.scss',
 })
-export class FormActividadesComponent {
+export class FormActividadesComponent extends BaseFormComponent<UserModel> {
+	@ViewChildren(FormTareasComponent) formularios!: QueryList<FormTareasComponent>;
 	TypeActivity: any;
-
+	datosTareas: any[] = [];
 	formulariosNombres: any[] = [];
-	protected _form!: FormGroup;
-	public get controls() {
-		return this._form?.controls;
+	override _service = inject(UserService);
+	constructor(private cdr: ChangeDetectorRef) {
+		super();
+		this.TypeActivity = [
+			{ name: 'Ordinaria' },
+			{ name: 'Extraordinaria' }
+		];
 	}
-
+	override buildForm(): void {
+		this._form = this._fb.group({
+			actividad: ['', Validators.required],
+			TipoActividad: ['', Validators.required],
+		})
+	}
 	agregarFormulario(): void {
 		this.formulariosNombres.push({});
 	}
@@ -29,10 +43,25 @@ export class FormActividadesComponent {
 	eliminarFormulario(index: number): void {
 		this.formulariosNombres.splice(index, 1);
 	}
-	ngOnInit() {
-		this.TypeActivity = [
-			{ name: 'Ordinaria' },
-			{ name: 'Extraordinaria' }
-		];
+	/* agregarFormulario(): void {
+		this.formulariosNombres.push({ hidden: false });
+	}
+
+	ocultarFormulario(i: number): void {
+		this.formulariosNombres[i].hidden = true;
+	} */
+	enviarDatos() {
+		const datosActividad = this.form.value;
+		this.datosTareas = this.formularios.map(f => f.getDatos());
+		const payload = {
+			actividad: datosActividad,
+			tareas: this.datosTareas
+		};
+		if (this.datosTareas.length > 0) {
+			console.log('Datos enviados:', payload);
+		} else {
+			console.error('Formulario inválido o sin tareas');
+		}
+
 	}
 }
--- src/app/modules/meets/pages/agenda/components/form-tareas/form-tareas.component.ts
+++ src/app/modules/meets/pages/agenda/components/form-tareas/form-tareas.component.ts
@@ -1,28 +1,53 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 import { DatePickerModule } from 'primeng/datepicker';
 import { InputTextModule } from 'primeng/inputtext';
 import { SelectModule } from 'primeng/select';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { UserService } from '@modules/users/services/user.service';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { UserParams } from '@modules/users/interfaces';
+import { UserModel, UserParams } from '@modules/users/interfaces';
 import { map, catchError, of, pipe } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { BaseFormComponent } from '@shared/components/abstracts/base-form.component';
+import { InputErrorComponent, InputTextComponent } from '@shared/components';
 
 @Component({
 	selector: 'app-form-tareas',
-	imports: [FormsModule, DatePickerModule, InputTextModule, SelectModule, MultiSelectModule],
+	imports: [InputTextComponent, InputErrorComponent, FormsModule, DatePickerModule, InputTextModule, SelectModule, MultiSelectModule, CommonModule, ReactiveFormsModule],
 	templateUrl: './form-tareas.component.html',
 	styleUrl: './form-tareas.component.scss',
 })
 export class FormTareasComponent {
+	_service = inject(UserService);
+	_form!: FormGroup;
+	_fb = inject(FormBuilder);
+	constructor() {
+		this.buildForm()
+	}
+	buildForm(): void {
+		this._form = this._fb.group({
+			tarea: ['', Validators.required],
+			fechas: [[], Validators.required],
+			resultado: ['', Validators.required],
+			grupo: [null, Validators.required],
+			responsable: [null, Validators.required],
+			participantes: [[]]
+		})
+
+	}
+	getDatos() {
+		return this._form.value;
+	}
 	private _userService = inject(UserService);
 	public Users = toSignal(this._userService.getAll(new UserParams().setShowAll(true).setSortField('name'))
 		.pipe(
 			map(res => res?.items ?? [],
 			),
 			catchError(() => of([])),
-
 		)
 	)
+	get controls() {
+		return this._form.controls;
+	}
 }
